5db72dd6f7ff7f7f395ea1793534c5e27c36fc26
baophan245 <01666119817fz@gmail.com>
2026-01-14 22:33:24 +0700

    Update GameWindow.java

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,1 +1,14 @@
+import javax.swing.*;
 
+public class GameWindow extends JFrame {
+    public GameWindow() {
+        setTitle("Flappy Bird Java");
+        setSize(400, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
