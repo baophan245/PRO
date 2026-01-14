ce631b3a28f2d0febb146094e528b48b9ab68c66
baophan245 <01666119817fz@gmail.com>
2026-01-14 22:28:52 +0700

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -1,1 +1,28 @@
-java
+package com.nhomxx.entities;
+
+public class Bird {
+    private int x, y;
+    private int velocity;
+
+    public Bird(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.velocity = 0;
+    }
+
+    public void flyUp() {
+        velocity = -15; // bay lên khi nhấn SPACE
+    }
+
+    public void applyGravity() {
+        velocity += 2; // trọng lực kéo xuống
+        y += velocity;
+    }
+
+    public int getY() { return y; }
+
+    @Override
+    public String toString() {
+        return "Bird at (" + x + "," + y + ")";
+    }
+}
