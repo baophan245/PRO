b99ead2265f079c099e842113ddb1c912d258b57
baophan245 <01666119817fz@gmail.com>
2026-01-14 22:32:52 +0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,1 +1,32 @@
+import java.util.ArrayList;
 
+public class Game {
+    private Bird bird;
+    private ArrayList<Pipe> pipes;
+    private int score;
+    private boolean isGameOver;
+
+    public Game() {
+        bird = new Bird(50, 200);
+        pipes = new ArrayList<>();
+        score = 0;
+        isGameOver = false;
+    }
+
+    public void update() {
+        bird.applyGravity();
+        for (Pipe pipe : pipes) {
+            pipe.moveLeft();
+            if (pipe.checkCollision(bird)) {
+                isGameOver = true;
+            }
+        }
+    }
+
+    public void restart() {
+        bird = new Bird(50, 200);
+        pipes.clear();
+        score = 0;
+        isGameOver = false;
+    }
+}
