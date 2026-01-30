2d605a3a535d6f93a1cbb82f4d8cb3316354ab94
baophan245 <01666119817fz@gmail.com>
2026-01-30 23:17:11 +0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,32 +1,19 @@
 import java.util.ArrayList;
 
 public class Game {
-    private Bird bird;
-    private ArrayList<Pipe> pipes;
-    private int score;
-    private boolean isGameOver;
+    public Bird bird = new Bird(50, 250);
+    public ArrayList<Pipe> pipes = new ArrayList<>();
+    public boolean over = false;
 
     public Game() {
-        bird = new Bird(50, 200);
-        pipes = new ArrayList<>();
-        score = 0;
-        isGameOver = false;
+        pipes.add(new Pipe(400, 0, 150)); // Ống trên
+        pipes.add(new Pipe(400, 300, 300)); // Ống dưới
     }
 
     public void update() {
-        bird.update(); 
-        for (Pipe pipe : pipes) {
-            pipe.update(); 
-            if (pipe.checkCollision(bird)) {
-                isGameOver = true;
-            }
-        }
-    }
-
-    public void restart() {
-        bird = new Bird(50, 200);
-        pipes.clear();
-        score = 0;
-        isGameOver = false;
+        if (over) return;
+        bird.update();
+        for (Pipe p : pipes) p.update();
+        if (bird.y > 600 || bird.y < 0) over = true; // Rơi khỏi màn hình là thua
     }
 }
