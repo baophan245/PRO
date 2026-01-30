ed3295990518262b9ee5c4e776cbe9156f938e7a
baophan245 <01666119817fz@gmail.com>
2026-01-30 23:16:24 +0700

    Update Movable.java

--- src/Movable.java
+++ src/Movable.java
@@ -1,1 +1,10 @@
+import java.awt.Graphics;
 
+public abstract class Movable extends GameObject {
+    protected int velocity;
+
+    public Movable(int x, int y, int w, int h, int v) {
+        super(x, y, w, h);
+        this.velocity = v;
+    }
+}
