a6ae58b263d04fb908fc3c015e6fe9f5ada43e18
baophan245 <01666119817fz@gmail.com>
2026-01-30 22:59:15 +0700

    Update GameObject.java

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,12 +1,11 @@
-
 public abstract class GameObject {
-    protected int x, y; 
+    protected int x, y;
 
     public GameObject(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-   
+ 
     public abstract void update();
 }
