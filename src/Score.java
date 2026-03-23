61c2aa590c53d8ac9feeb9ebf1e047aa74d64e2a
lx426352-art <lx426352@gmail.com>
2026-03-23 16:05:36 +0700

    Update Score.java

--- src/Score.java
+++ src/Score.java
@@ -1,1 +1,20 @@
+public class Score {
+    private int currentScore;
+    private static int highScore = 0;
 
+    public Score() {
+        this.currentScore = 0;
+    }
+
+    public void increment() {
+        this.currentScore++;
+        if (this.currentScore > highScore) {
+            highScore = this.currentScore;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Current Score: " + currentScore + " | High Score: " + highScore;
+    }
+}
