9bb168df90539b4b95c02a385144a9923793df2d
lx426352-art <lx426352@gmail.com>
2026-03-23 16:07:58 +0700

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -1,20 +1,28 @@
-import java.awt.Color;
-import java.awt.Graphics;
+public class Bird extends GameObject {
 
-public class Bird extends Movable {
-    public Bird(int x, int y) { super(x, y, 30, 30, 0); }
+    private int velocity;
 
-    public void fly() { velocity = -15; } // Nhảy lên
+    public Bird(int x, int y, int velocity) {
+        super(x, y);  
+        this.velocity = velocity;
+    }
 
-    @Override
-    public void update() {
-        velocity += 1; // Trọng lực
-        y += velocity;
+    public int getVelocity() {
+        return this.velocity;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
     }
 
     @Override
-    public void draw(Graphics g) {
-        g.setColor(Color.RED);
-        g.fillOval(x, y, width, height); // Vẽ con chim hình tròn màu đỏ
+    public void move() {
+        this.setY(this.getY() - this.velocity);
+    }
+
+    @Override
+    public String toString() {
+        return "Bird - " + super.toString() + 
+               " - Velocity: " + this.velocity;
     }
 }
