0b9476d28cd0fa386593d0d8aabc29564f0a66ca
lx426352-art <lx426352@gmail.com>
2026-03-23 16:07:02 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,27 @@
+import java.util.ArrayList;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<GameObject> objectList = new ArrayList<>();
+
+        objectList.add(new Bird(100, 200, 5));
+        objectList.add(new Pipe(300, 150, 10));
+        objectList.add(new Bird(120, 220, 8));
+        objectList.add(new Pipe(400, 180, 6));
+
+        System.out.println("=== Before Move ===");
+        for (GameObject object : objectList) {
+            System.out.println(object);
+        }
+
+        System.out.println("=== After Move ===");
+        for (GameObject object : objectList) {
+
+            object.move();
+
+            System.out.println(object);
+        }
+    }
+}
