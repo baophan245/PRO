438427354d6afc70c5e1493ab9c278b74c8711a9
lx426352-art <lx426352@gmail.com>
2026-03-23 16:07:41 +0700

    Implement GameManager with object management and scoring

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
 
+public class GameManager {
+    private ArrayList<GameObject> objects;
+    private Score gameScore;
+
+    public GameManager() {
+        objects = new ArrayList<>();
+        gameScore = new Score();
+    }
+
+    public void addObject(GameObject obj) {
+        objects.add(obj);
+    }
+
+    public void updateGame() {
+        System.out.println("--- Updating Game State ---");
+        for (GameObject obj : objects) {
+            obj.move(); 
+            System.out.println(obj); 
+        }
+        gameScore.increment(); 
+        System.out.println(gameScore);
+    }
+}
