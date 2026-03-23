c3f7996096aa07f7bc742ef411f451909e78b2f3
lx426352-art <lx426352@gmail.com>
2026-03-23 16:07:20 +0700

    Update GameObject.java

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,12 +1,34 @@
-import java.awt.Graphics;
-
 public abstract class GameObject {
-    protected int x, y, width, height;
 
-    public GameObject(int x, int y, int w, int h) {
-        this.x = x; this.y = y; this.width = w; this.height = h;
+    private int x;
+    private int y;
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public abstract void update();
-    public abstract void draw(Graphics g);
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+   
+    public abstract void move();
+
+    @Override
+    public String toString() {
+        return "Position: " + this.x + ", " + this.y;
+    }
 }
