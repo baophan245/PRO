9ce6dba8cc4a05fb3053db05427472bf07db1259
lx426352-art <lx426352@gmail.com>
2026-03-23 16:06:44 +0700

    Update Movable.java

--- src/Movable.java
+++ src/Movable.java
@@ -1,10 +1,3 @@
-import java.awt.Graphics;
-
-public abstract class Movable extends GameObject {
-    protected int velocity;
-
-    public Movable(int x, int y, int w, int h, int v) {
-        super(x, y, w, h);
-        this.velocity = v;
-    }
+public interface Movable {
+    void move();
 }
